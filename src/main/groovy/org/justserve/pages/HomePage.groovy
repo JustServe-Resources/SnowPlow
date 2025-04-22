17757822a4b058e64542caa9ca7f47ad2ea8f07e
BryanDyer <bryan.dyer@churchofjesuschrist.org>
2025-04-22 15:56:06 -0600

    feat: add soonest carousel selector
    add selector for card locations

--- src/main/groovy/org/justserve/pages/HomePage.groovy
+++ src/main/groovy/org/justserve/pages/HomePage.groovy
@@ -9,5 +9,14 @@ class HomePage extends JustServePage {
 
     @SuppressWarnings("unused")
     static at = { (title == "JustServe") }
+    static content = {
+        //soonestCarouselNextButton { $(By.xpath("//div[starts-with(@class,\"soonest section\")]//button[@aria-label=\"Next\"]")) }
+        //soonestCarouselPreviousButton { $(By.xpath("//div[starts-with(@class,\"soonest section\")]//button[@aria-label=\"Previous\"]")) }
+        projectCardLocations { $(By.xpath("//div[@class=\"soonest section desktop\"]//div[@class=\"project-card-location\"]")) }
+    }
+
+//    String[][] getSoonestProjectLocations() {
+
+//    }
 }
 
